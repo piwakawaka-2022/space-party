4f616c2ba46e54be8ec081547f3f1cd84eb1177a
Lawrence Goodhead <92251452+Lawrence-gg@users.noreply.github.com>
2022-04-14 23:43:09 +1200

    added todos for tomorrow morning in space api file

--- client/api/spaceApi.js
+++ client/api/spaceApi.js
@@ -1,5 +1,12 @@
 import request from 'superagent'
 
+//TODOs
+//1: Add utils folder -> random day 2010-2021, 01-12, 0-28
+//add &date=RANDOMDATE  to nasa api picture of day to make it a random pic
+// ask seb to link me the miro board, lol
+
+//2: Add second nasa API -> mars rover camera -> find good mars sol with nice images
+// Images are mostly gray scale (from mars rover api), maybe a 'grayscale class' to images pulled from this?
 
 export function getSpacePictureOfTheDay() {
   return request
